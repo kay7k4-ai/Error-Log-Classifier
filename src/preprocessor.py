357a21cbd2f0d65c101ad8b79de2f74ee51f5330
Karima Khatun <kay7k4@gmail.com>
2025-11-20 12:54:14 +0530

    Day 1

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -4,7 +4,7 @@ def read_logs(file_path):
     return logs
 
 if __name__ == "__main__":
-    logs = read_logs("data/sample_logs.txt")
+    logs = read_logs("../data/sample_logs.txt")
     print("Raw log lines:")
     for line in logs:
         print(line.strip())
